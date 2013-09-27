563b352a2d30aed1f662a644a950254f6bc4cf76
pierre-arnaud lumalé <kadactivity@gmail.com>
2013-09-27 21:51:18 +0200

    EAK installation
    npm install
    bower install
    grunt server

---
+++ app/app.js
@@ -0,0 +1,10 @@
+import Resolver from 'resolver';
+
+var App = Ember.Application.extend({
+  LOG_ACTIVE_GENERATION: true,
+  LOG_VIEW_LOOKUPS: true,
+  modulePrefix: 'appkit', // TODO: loaded via config
+  Resolver: Resolver
+});
+
+export default App;
