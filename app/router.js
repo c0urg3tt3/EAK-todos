173f7f3b51c3212d9bae3e18d3e7d3a855de62c9
pierre-arnaud lumalé <kadactivity@gmail.com>
2013-09-30 14:24:30 +0200

    Adding Child Route

--- app/router.js
+++ app/router.js
@@ -2,7 +2,9 @@
 var Router = Ember.Router.extend(); // ensure we don't share routes between all Router instances
 
 Router.map(function(){
-  this.resource('todos', { path: '/' });
+  this.resource('todos', { path: '/' }, function() {
+  
+  });
 });
 
 export default Router;
