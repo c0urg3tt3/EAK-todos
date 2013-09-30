ea6f0165d67b2b0285d0c2d37b6e0dec4bc86cd3
pierre-arnaud lumalé <kadactivity@gmail.com>
2013-09-30 14:30:11 +0200

    Transitioning to Show Only Incomplete Todos

--- app/router.js
+++ app/router.js
@@ -3,7 +3,7 @@ var Router = Ember.Router.extend(); // ensure we don't share routes between all 
 
 Router.map(function(){
   this.resource('todos', { path: '/' }, function() {
-  
+    this.route('active');
   });
 });
 
---
+++ app/routes/todos/active.js
@@ -0,0 +1,12 @@
+var TodosActiveRoute = Ember.Route.extend({
+  model: function(){
+    return this.store.filter('todo', function (todo) {
+      return !todo.get('isCompleted');
+    });
+  },
+  renderTemplate: function(controller){
+    this.render('todos/index', {controller: controller});
+  }
+});
+
+export default TodosActiveRoute;
